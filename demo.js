a6b8298ad6a64be0b4654f9c363f1e584010c2dc
Tim <timprice@mangoraft.com>
2014-06-06 21:27:27 -0500

    update code

--- demo.js
+++ demo.js
@@ -1,20 +1,17 @@
 var usage = require('usage');
 
+
 var udpServer = require('./lib/udpserver').createServer({
 	host : '127.0.0.1',
 	port : 4001,
-	mongodb : {
-		host : '127.0.0.1'
-	}
+	mongodb : mongodb
 });
 
 var webServer = require('./lib/webserver').createServer({
 	udp : udpServer,
 	host : '127.0.0.1',
-	port : 4002,
-	mongodb : {
-		host : '127.0.0.1'
-	}
+	port : 4001,
+	mongodb : mongodb
 });
 
 var memory = require('./lib/metric').createMetric({
